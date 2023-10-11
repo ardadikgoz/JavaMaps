b6aee44e1271e4cf2d6244b9198dae81055011c5
arda <arda.dikgoz@bil.omu.edu.tr>
2023-10-12 02:56:45 +0300

    v1.0

---
+++ app/src/main/java/com/ardadikgoz/javamaps/adapter/PlaceAdapter.java
@@ -0,0 +1,60 @@
+package com.ardadikgoz.javamaps.adapter;
+
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ardadikgoz.javamaps.MapsActivity;
+import com.ardadikgoz.javamaps.databinding.RecyclerRowBinding;
+import com.ardadikgoz.javamaps.model.Place;
+
+import java.util.List;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder> {
+
+    List<Place> placeList;
+
+    public PlaceAdapter(List<Place> placeList) {
+        this.placeList = placeList;
+    }
+
+    @NonNull
+    @Override
+    public PlaceHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PlaceHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceAdapter.PlaceHolder holder, int position) {
+        holder.recyclerRowBinding.recyclerViewTextView.setText(placeList.get(position).name);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(), MapsActivity.class);
+                placeList.get(holder.getAdapterPosition());
+                intent.putExtra("info","old");
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return placeList.size();
+    }
+
+    public class PlaceHolder extends RecyclerView.ViewHolder{
+        RecyclerRowBinding recyclerRowBinding;
+        public PlaceHolder(RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+    }
+}
---
+++ app/src/main/java/com/ardadikgoz/javamaps/roomdb/PlaceDatabase.java
@@ -0,0 +1,11 @@
+package com.ardadikgoz.javamaps.roomdb;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.ardadikgoz.javamaps.model.Place;
+
+@Database(entities = {Place.class},version = 1)
+public abstract class PlaceDatabase extends RoomDatabase {
+    public abstract PlaceDao placeDao();
+}
